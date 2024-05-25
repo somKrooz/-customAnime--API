f425e1fdb02071ec92255a687aa2ecb3d06e2be3
somKrooz <somKrooz80@gmail.com>
2024-05-25 19:36:02 +0530

    (customAnime)-API

---
+++ data.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+##d
+
+data = pd.read_csv("csv/animes.csv")
+def getData(title: str):
+    filtered_data = data[data["title"] == title]
+
+    sorted_data = filtered_data.iloc[0]
+
+    api_end = {
+        "title": sorted_data['title'],
+        "genre": sorted_data['genre'],
+        "episodes": sorted_data['episodes'],
+        "image": sorted_data['img_url'],
+        "url":  sorted_data['link'],
+        "rank": sorted_data["ranked"],
+        "synopsis:" : sorted_data['synopsis'],
+        "aired:" : sorted_data['aired']
+    }
+
+    return api_end
+
+def getAll():
+    global_Num = 20
+    all_data = data
+    api_data = {
+        "title": all_data["title"][:global_Num],
+        "genre": all_data["genre"][:global_Num],
+        "episodes": all_data["episodes"][:global_Num],
+        "score": all_data["score"][:global_Num],
+        "image": all_data['img_url'][:global_Num],
+    }
+
+    return api_data
+
+
+def getBranched():
+    new_data  = data[:100]
+    cool = {}
+    for index, row in new_data.iterrows():
+        Krooz = {
+            "title": row["title"],
+            "image": row["img_url"],
+        }
+
+        cool[index] = Krooz
+
+    return cool
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from data import getData , getAll , getBranched
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    isCool: Optional[bool] =True
+
+#Give me my money
+@app.get('/')
+async def root():
+    return {"Hello": "World"}
+
+@app.get('/krooz')
+async def krooz():
+   json = getAll()
+   return json
+
+
+@app.get('/post')
+async def post(title):
+    json = getData(title)
+    return json
+
+@app.get("/anime")
+async def anime():
+    json = getBranched()
+    return json
+
+
